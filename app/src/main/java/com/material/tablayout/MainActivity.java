155542fb46663570ff3b813b24de46d084fde308
Rohit882333 <rohitkumar882333@gmail.com>
2020-10-08 16:59:06 +0530

    first commit

---
+++ app/src/main/java/com/material/tablayout/MainActivity.java
@@ -0,0 +1,32 @@
+package com.material.tablayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.os.Bundle;
+
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity 
+{
+    private Toolbar toolbar;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+		
+        
+    }
+    @Override
+		public boolean onCreateOptionsMenu(Menu menu) {
+				MenuInflater inflater = getMenuInflater();
+				inflater.inflate(R.menu.main_menu, menu);
+				return super.onCreateOptionsMenu(menu);
+		}
+}
